2e0aa625ef4db7278c6989fe639c1a4d4f0e94a0
Eduardo Dimas <eddimas@gmail.com>
2025-03-05 11:00:08 -0600

    Add delete lambda

--- terraform/main/deployment.tf
+++ terraform/main/deployment.tf
@@ -1,7 +1,8 @@
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_method.get_method,
-    aws_api_gateway_method.post_method
+    aws_api_gateway_method.post_method,
+    aws_api_gateway_method.delete_method
   ]
 
   rest_api_id = aws_api_gateway_rest_api.device_event_api.id
