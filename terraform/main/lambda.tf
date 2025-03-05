cdc86cd07fdb41fc9628883ae6bcfd8a474c9c95
Eduardo Dimas <eddimas@gmail.com>
2025-03-05 01:33:20 -0600

    Organize lambda structure

--- terraform/main/lambda.tf
+++ terraform/main/lambda.tf
@@ -1,19 +1,23 @@
-module "lambda_function" {
-  source = "${path.module}/modules/lambda_function"
+# Lambda function configurations
+data "aws_lambda_function" "lambda_configs" {
+  for_each      = toset(["get", "post", "delete"])
+  function_name = each.key
+}
 
-  for_each = {
-    get    = "get"
-    post   = "post"
-    delete = "delete"
-  }
+# Create Lambda functions recursively
+resource "aws_lambda_function" "lambda_functions" {
+  for_each = data.aws_lambda_function.lambda_configs
 
   function_name = each.key
-  runtime       = var.runtime
+  runtime       = "python3.8"
   handler       = "${each.key}/device_data.lambda_handler"
-  s3_bucket     = var.bucket_data_name
-  s3_key        = "${each.value}.zip"
+  s3_bucket     = aws_s3_bucket.lambda_bucket.id
+  s3_key        = "${each.key}.zip"
   role          = aws_iam_role.lambda_exec.arn
-  environment_variables = {
-    DYNAMODB_TABLE = aws_dynamodb_table.processed_data.name
+
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.processed_data.name
+    }
   }
 }
