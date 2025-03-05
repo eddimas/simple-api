ddd276bac165a6898db7f2330d02bdd8da0c9240
Eduardo Dimas <eddimas@gmail.com>
2025-03-05 14:08:44 -0600

    Add delete lambda

--- terraform/main/api_gateway.tf
+++ terraform/main/api_gateway.tf
@@ -42,7 +42,7 @@ resource "aws_api_gateway_method" "post_method" {
 resource "aws_api_gateway_integration" "post_integration" {
   rest_api_id             = aws_api_gateway_rest_api.device_event_api.id
   resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.delete_method.http_method
+  http_method             = aws_api_gateway_method.post_method.http_method
   type                    = "AWS_PROXY"
   integration_http_method = "POST" # ✅ FIXED
   uri                     = aws_lambda_function.lambda_functions["post"].invoke_arn
