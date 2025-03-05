87940826d6676175edcfe86c8a001e0c278305f3
Eduardo Dimas <eddimas@gmail.com>
2025-03-05 16:01:00 -0600

    Add logs to trace

--- terraform/main/api_gateway.tf
+++ terraform/main/api_gateway.tf
@@ -101,16 +101,16 @@ resource "aws_api_gateway_deployment" "deployment" {
 
 
 
-resource "aws_api_gateway_deployment" "deployment" {
-  depends_on = [
-    aws_api_gateway_method.get_method,
-    aws_api_gateway_method.post_method,
-    aws_api_gateway_method.delete_method
-  ]
+# resource "aws_api_gateway_deployment" "deployment" {
+#   depends_on = [
+#     aws_api_gateway_method.get_method,
+#     aws_api_gateway_method.post_method,
+#     aws_api_gateway_method.delete_method
+#   ]
 
-  rest_api_id = aws_api_gateway_rest_api.device_event_api.id
-  stage_name  = var.api_gw_stg_name
-}
+#   rest_api_id = aws_api_gateway_rest_api.device_event_api.id
+#   stage_name  = var.api_gw_stg_name
+# }
 
 
 
