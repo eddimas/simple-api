ce8d8117326c82b4eeef832a984854a8f7369b3a
Eduardo Dimas <eddimas@gmail.com>
2025-03-05 22:59:50 -0600

    Add delete lambda

--- terraform/main/api_gateway.tf
+++ terraform/main/api_gateway.tf
@@ -29,9 +29,6 @@ resource "aws_api_gateway_stage" "apigw_stage" {
   }
 }
 
-resource "aws_cloudwatch_log_group" "api_gw_logs" {
-  name = "/aws/apigateway/${aws_api_gateway_rest_api.device_event_api.name}"
-}
 
 resource "aws_iam_role" "api_gateway_logging_role" {
   name = "api_gateway_logging_role"
--- terraform/main/cloudwatch.tf
+++ terraform/main/cloudwatch.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "api_gw_logs" {
-  name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.device_event_api.id}/${var.stage_name}"
+  name = "/aws/apigateway/${aws_api_gateway_rest_api.device_event_api.name}"
+  #name              = "API-Gateway-Execution-Logs_${aws_api_gateway_rest_api.device_event_api.id}/${var.stage_name}"
   retention_in_days = 7
-
 }
