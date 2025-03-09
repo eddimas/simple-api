b5e32c17ed3f74f31e12efcf254bbe52f95d14f7
Eduardo Dimas <eddimas@gmail.com>
2025-03-09 15:03:07 -0500

    Adding EVD feature

--- terraform/main/event_drive.tf
+++ terraform/main/event_drive.tf
@@ -1,13 +1,18 @@
+# Data Source for Existing S3 Bucket (Ensures the bucket exists)
+data "aws_s3_bucket" "device_csv_data_bucket" {
+  bucket = var.device_csv_data_bucket
+}
 
-resource "aws_lambda_function" "s3_processor" {
+# Lambda Function: Extract & Transform (Triggered by S3)
+resource "aws_lambda_function" "extract_transform" {
   function_name = "extract_transform"
   runtime       = var.runtime
   role          = aws_iam_role.lambda_exec.arn
   handler       = "extract_transform.lambda_handler"
   s3_bucket     = var.bucket_data_name
-  //filename      = "lambda_function.zip"
-  s3_key  = "extract_transform.zip"
-  timeout = 30
+  s3_key        = "extract_transform.zip"
+  timeout       = 30
+
   environment {
     variables = {
       SNS_TOPIC_ARN = aws_sns_topic.event_notifications.arn
@@ -15,15 +20,16 @@ resource "aws_lambda_function" "s3_processor" {
   }
 }
 
-resource "aws_lambda_function" "sns_to_dynamodb" {
+# Lambda Function: Load (Triggered by SNS and writes to DynamoDB)
+resource "aws_lambda_function" "load" {
   function_name = "load"
   runtime       = var.runtime
   role          = aws_iam_role.lambda_exec.arn
   handler       = "load.lambda_handler"
   s3_bucket     = var.bucket_data_name
-  //filename      = "lambda_function.zip"
-  s3_key  = "load.zip"
-  timeout = 30
+  s3_key        = "load.zip"
+  timeout       = 30
+
   environment {
     variables = {
       DYNAMODB_TABLE = aws_dynamodb_table.processed_data.name
@@ -31,23 +37,23 @@ resource "aws_lambda_function" "sns_to_dynamodb" {
   }
 }
 
-data "aws_s3_bucket" "device_csv_data_bucket" {
-  bucket = var.device_csv_data_bucket
-}
-
-resource "aws_s3_bucket_notification" "s3_event_trigger" {
-  bucket = var.device_csv_data_bucket
-
-  lambda_function {
-    lambda_function_arn = aws_lambda_function.s3_processor.arn
-    events              = ["s3:ObjectCreated:*"]
-  }
-}
-
+# Lambda Permission to Allow S3 to Invoke extract_transform Function
 resource "aws_lambda_permission" "allow_s3" {
   statement_id  = "AllowExecutionFromS3"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.s3_processor.function_name
+  function_name = aws_lambda_function.extract_transform.function_name
   principal     = "s3.amazonaws.com"
   source_arn    = data.aws_s3_bucket.device_csv_data_bucket.arn
+}
+
+# S3 Event Notification to Trigger extract_transform Lambda on Object Creation
+resource "aws_s3_bucket_notification" "s3_event_trigger" {
+  bucket = data.aws_s3_bucket.device_csv_data_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.extract_transform.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+
+  depends_on = [aws_lambda_permission.allow_s3]
 }
